7e81100375830e7b38b7c96bba1a2610cefc0539
Matthew Schwartzkopf <matthewschwartzkopf@stu-241-146-171-184.champlain.edu>
2017-04-17 19:11:40 -0400

    Initial Commit

---
+++ Blackjack/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Blackjack
+//
+//  Created by Matthew Schwartzkopf on 4/17/17.
+//  Copyright © 2017 Matthew Schwartzkopf. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
