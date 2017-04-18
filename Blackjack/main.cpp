dade77c167e84cab311c7475e9b9c9951d8e4b0a
MattSchwartzkopf <schwartzkopfm@gmail.com>
2017-04-18 11:41:11 -0400

    Test

--- Blackjack/main.cpp
+++ Blackjack/main.cpp
@@ -1,15 +1,11 @@
-//
-//  main.cpp
-//  Blackjack
-//
-//  Created by Matthew Schwartzkopf on 4/17/17.
-//  Copyright © 2017 Matthew Schwartzkopf. All rights reserved.
-//
-
 #include <iostream>
+#include <string>
+#include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+
+int main (void)
+{
+
 }
